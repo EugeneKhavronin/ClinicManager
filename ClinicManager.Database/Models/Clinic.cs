0a7082eb06c3bf899c598f27581b7e1917bbe766
EugeneKhavronin <zhekss66@gmail.com>
2019-07-02 11:33:36 +0300

    Добавлены модели

--- ClinicManager.Database/Models/Clinic.cs
+++ ClinicManager.Database/Models/Clinic.cs
@@ -1,7 +1,47 @@
+using System;
+
 namespace ClinicManager.Database.Models
 {
     public class Clinic
     {
+        /// <summary>
+        /// Уникальный идентификатор
+        /// </summary>
+        public Guid ClinicId { get; set; }
         
+        /// <summary>
+        /// Название клиники
+        /// </summary>
+        public string Title { get; set; }
+        
+        /// <summary>
+        /// Адрес клиники
+        /// </summary>
+        public string Adress { get; set; }
+
+        /// <summary>
+        /// Номер телефона
+        /// </summary>
+        public string PhoneNumber { get; set; }
+        
+        /// <summary>
+        /// Сайт клиники
+        /// </summary>
+        public string Url { get; set; }
+        
+        /// <summary>
+        /// Электронная почта клиники
+        /// </summary>
+        public string Email { get; set; }
+        
+        /// <summary>
+        /// Специализация клиники
+        /// </summary>
+        public string Specialisation { get; set; }
+        
+        /// <summary>
+        /// Уникальный идентификатор изображения
+        /// </summary>
+        public Guid PictureId { get; set; }
     }
 }
--- ClinicManager.Database/Models/Picture.cs
+++ ClinicManager.Database/Models/Picture.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace ClinicManager.Database.Models
 {
     public class Picture
     {
-        
+        public Guid PictureId { get; set; }
+
+        public string Title { get; set; }
+
+        public byte[] ClinicPicture { get; set; }
     }
 }
