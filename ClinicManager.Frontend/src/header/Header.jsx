76ab3afb38cf41624ca3ce120d5c8467fbc48121
Alexandra1770 <sashashukova6123@gmail.com>
2019-07-10 15:11:35 +0300

    Merge remote-tracking branch 'remotes/origin/Work_Tsvetkov' into Work-Shukova
    # Conflicts:
    #	ClinicManager.Frontend/src/App.jsx

---
+++ ClinicManager.Frontend/src/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header-back">
+      <h1>Clinic Manager</h1>
+      <button type="button" className="button-add">
+        Добавить клинику
+      </button>
+    </header>
+  );
+};
+
+export default Header;
