c6e5dd0bd08a573e80aba5c3b37f3c87b497cf59
NikitaDurkin <spartaknikita007@mail.ru>
2019-07-11 12:02:34 +0300

    Clinic ClinicGuid -> Guid, Required, Конструкторы, XML Picture PictureGuid -> Guid, Required, Конструкторы, XML

--- ClinicManager.Database/Models/Clinic.cs
+++ ClinicManager.Database/Models/Clinic.cs
@@ -13,41 +13,75 @@ namespace ClinicManager.Database.Models
         /// Уникальный идентификатор
         /// </summary>
         [Key]
-        public Guid ClinicGuid { get; set; }
-        
+        public Guid Guid { get; set; }
+
         /// <summary>
         /// Название клиники
         /// </summary>
+        [Required]
         public string Title { get; set; }
-        
+
         /// <summary>
         /// Адрес клиники
         /// </summary>
+        [Required]
         public string Address { get; set; }
 
         /// <summary>
         /// Номер телефона
         /// </summary>
+        [Required]
         public string PhoneNumber { get; set; }
-        
+
         /// <summary>
         /// Сайт клиники
         /// </summary>
         public string Url { get; set; }
-        
+
         /// <summary>
         /// Электронная почта клиники
         /// </summary>
         public string Email { get; set; }
-        
+
         /// <summary>
         /// Специализация клиники
         /// </summary>
+        [Required]
         public string Specialisation { get; set; }
-        
+
         /// <summary>
         /// Уникальный идентификатор изображения
         /// </summary>
-       public Guid PictureGuid { get; set; }
+        public Guid PictureGuid { get; set; }
+
+        /// <summary>
+        /// конструктор модели
+        /// </summary>
+        public Clinic()
+        {
+        }
+
+        /// <summary>
+        /// Конструктор модели
+        /// </summary>
+        /// <param name="title">Название клиники</param>
+        /// <param name="address">Адрес клиники</param>
+        /// <param name="phoneNumber">Номер телефона</param>
+        /// <param name="url">Сайт клиники</param>
+        /// <param name="email">Электронная почта клиники</param>
+        /// <param name="specialisation">Специализация клиники</param>
+        /// <param name="guid">Уникальный идентификатор изображения</param>
+        public Clinic(string title, string address, string phoneNumber, string url, string email, string specialisation,
+            Guid guid)
+        {
+            Guid = Guid.NewGuid();
+            title = Title;
+            address = Address;
+            phoneNumber = PhoneNumber;
+            url = Url;
+            email = Email;
+            specialisation = Specialisation;
+            guid = PictureGuid;
+        }
     }
 }
--- ClinicManager.Database/Models/Picture.cs
+++ ClinicManager.Database/Models/Picture.cs
@@ -7,30 +7,52 @@ namespace ClinicManager.Database.Models
     /// <summary>
     /// Модель фотографии
     /// </summary>
-    public class Picture 
+    public class Picture
     {
         /// <summary>
         /// Уникальный идентификатор фотографии
         /// </summary>
         [Key]
-        public Guid PictureGuid { get; set; }
-        
+        public Guid Guid { get; set; }
+
         /// <summary>
         /// Название фотографии
         /// </summary>
         public string Title { get; set; }
-        
+
         /// <summary>
         /// Фотография клиники
         /// </summary>
         public byte[] ClinicPicture { get; set; }
-        
+
         /// <summary>
         /// Связи между таблицами
         /// </summary>
         [ForeignKey("Clinic")]
         public Guid ClinicGuid { get; set; }
+
+        /// <summary>
+        /// Связи между таблицами
+        /// </summary>
         public Clinic ClinicPic { get; set; }
 
+        /// <summary>
+        /// Конструктор модели
+        /// </summary>
+        public Picture()
+        {
+        }
+
+        /// <summary>
+        /// Конструктор модели
+        /// </summary>
+        /// <param name="title">Название фотографии</param>
+        /// <param name="clinicPicture">Фотография клиники</param>
+        public Picture(string title, byte[] clinicPicture)
+        {
+            Guid = Guid.NewGuid();
+            title = Title;
+            clinicPicture = ClinicPicture;
+        }
     }
 }
